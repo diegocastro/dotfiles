5fff4759f0c78ed7e44a962e14d4324d7168a0ea
Diego Castro <castroc.diego@gmail.com>
2015-10-21 11:25:23 -0200

    bugfix ls alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,2 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="ls -F --color"
-  alias la="ls -lAh --color"
-fi
+alias ls="ls -F --color"
+alias lsa="ls -lAh --color"
