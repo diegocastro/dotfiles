4ee1e9cd781993a43a230e88af42227f32ab9c18
Diego Castro <castroc.diego@gmail.com>
2015-10-23 16:30:44 -0200

    better aliases for php

---
+++ php/aliases.zsh
@@ -0,0 +1,6 @@
+# Symfony 2 commands completion
+
+alias sf='php app/console'
+alias sfdmd='sf doctrine:migrations:diff'
+alias sfdmm='sf doctrine:migrations:migrate'
+alias sfdgf='sf doctrine:generate:form'
--- symfony2/aliases.zsh
+++
@@ -1,6 +0,0 @@
-# Symfony 2 commands completion
-
-alias sf='php app/console'
-alias sfdf='sf doctrine:migrations:diff'
-alias sfdm='sf doctrine:migrations:migrate'
-alias sfgf='sf doctrine:generate:form'
