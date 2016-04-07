c13d33e461a5362fa127636086824c657c45f858
Diego Castro <castroc.diego@gmail.com>
2016-04-07 12:16:14 -0300

    git pull rebase alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias gc='git commit'
 alias gcm='git commit -m'
 alias gco='git checkout'
 alias gp='git push origin HEAD'
+alias gpr='git pull --rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 
 # php
