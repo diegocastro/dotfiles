3a3dc14c915c50b1dc5750ca29371c7babaf7125
Diego Castro <castroc.diego@gmail.com>
2016-09-19 21:23:00 -0300

    better prompt color

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -25,6 +25,6 @@ if [ $UID -eq 0 ]; then CARETCOLOR="red"; else CARETCOLOR="blue"; fi
 
 local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
 
-PROMPT='%m %{${fg_bold[blue]}%}:: %{$reset_color%}%{${fg[green]}%}%3~ $(git_dirty)%{${fg_bold[$CARETCOLOR]}%}»%{${reset_color}%} '
+PROMPT='%{$fg_bold[white]%}%n@%m%{$reset_color%} %{${fg_bold[blue]}%}:: %{$reset_color%}%{${fg[green]}%}%3~ $(git_dirty)%{${fg_bold[$CARETCOLOR]}%}»%{${reset_color}%} '
 
 RPS1="${return_code}"
